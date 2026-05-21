a73095047142818cb67aa59ca16595b9e1975a56
scalibur123 <mario.abascal.acebal@gmail.com>
2026-05-21 17:45:24 +0200

    Delete negocio/js/supabase.js

--- negocio/js/supabase.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://yxmirhpwmdmaxynlshdx.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4bWlyaHB3bWRtYXh5bmxzaGR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxODc2MDcsImV4cCI6MjA5Mzc2MzYwN30.tCJKmAeG9A3ua1WJ5s166OvnIMalfS9SLOA23McirGc';
-
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
